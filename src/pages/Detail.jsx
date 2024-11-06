a5c8b75a99ef73ddce6f894f4b525c3a3394c45b
hanhyochan <hcartist569@gmail.com>
2024-11-06 17:03:03 +0900

    11/06

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -5,14 +5,14 @@ const Detail = () => {
     const movieDetailData = useSelector(state => state.movieDetail)
     // const [movie, setMovie] = useState(movieDetailData)
     const genres = movieDetailData.genres
-console.log(movieDetailData)
+console.log(movieDetailData.overView)
     return (
         <div>
            <img src={movieDetailData.img} />
            <h1>{movieDetailData.title}</h1>
            <span>{movieDetailData.voteAverage}</span>
            <span>{genres.map((el) => <span key={el.id}>{el.name}</span>)}</span>
-           <span>{movieDetailData.overview}</span>
+           <span>{movieDetailData.overView}</span>
         </div>
     );
 }; 
