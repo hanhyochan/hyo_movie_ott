6d84503e2ebc50afa3f1736cca746fce1797ef06
hanhyochan <hcartist569@gmail.com>
2024-11-08 01:58:22 +0900

    11/08

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
 import { useState, useEffect } from 'react'
 import './App.scss'
-import.meta.env.VITE_API_KEY
 import MovieCard from './component/MovieCard'
 import { Route, Routes } from 'react-router-dom'
 import MovieDetail from './pages/MovieDetail'
 import NavBar from './component/NavBar '
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+const API_KEY = import.meta.env.VITE_API_KEY
 
 function App() {
   const [movieList, setMovieList] = useState([])
 
+  const MOVIE_URL = 'https://api.themoviedb.org/3/movie/popular';
   const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=9a3d28aa764f7a0dd23e10e216af9d90');
-          const movieListData = await response.json();
-          setMovieList(movieListData.results.map((el) => ({
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(`${MOVIE_URL}?api_key=${API_KEY}&language=ko-KR&page=1`)
+        .then((res) => res.json())
+        .then((res) => {
+          setMovieList(res.results.map((el) => ({
             id: el.id,
             img: BASE_IMAGE_URL + el.poster_path,
             title: el.title,
             voteAverage: el.vote_average
           })))
-        } catch (error) {
-          console.error('데이터 에러', error);
-        }
-      };
-      fetchData();
-    }, []);
+        })
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-    <NavBar />
-    ff
+      <NavBar />
+      ff
       <Routes>
         <Route path='/' element={<div className='container'>
           {movieList.map((el) => <MovieCard key={el.id} movieList={el} />)}
@@ -43,7 +42,7 @@ function App() {
         <Route path='/signIn' element={<SignIn />} />
         <Route path='/signUp' element={<SignUp />} />
       </Routes >
-      </>
+    </>
   )
 }
 
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,45 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import.meta.env.VITE_API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY
 
 const Detail = () => {
     const [movie, setMovie] = useState([])
-const { movieId } = useParams()
+    const { movieId } = useParams()
 
+    const MOVIE_URL = 'https://api.themoviedb.org/3/movie/';
     const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=9a3d28aa764f7a0dd23e10e216af9d90`);
-            const movieDetailData = await response.json();
-            setMovie({
-                posterImg: `${BASE_IMAGE_URL}${movieDetailData.belongs_to_collection.poster_path}`,
-                backgroundImg: `${BASE_IMAGE_URL}${movieDetailData.backdrop_path}`,
-                title: movieDetailData.title,
-                runtime: movieDetailData.runtime,
-                genres: movieDetailData.genres.map((el, index, array) => {
-                    if (index !== array.length - 1) {
-                        return el.name + ' | ';
-                    } else {
-                        return el.name;
-                    }
-                }).join(''),
-                releaseDate: movieDetailData.release_date,
-                voteAverage: movieDetailData.vote_average,
-                overView: movieDetailData.overview,
-            })
-          } catch (error) {
-            console.error('데이터 에러', error);
-          }
-        };
-        fetchData();
-      }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //       try {
+    //         const response = await fetch(`https://api.themoviedb.org/3/movie/popular/${movieId}?api_key=${API_KEY}`);
+    //         const movieDetailData = await response.json();
+    //         console.log(movieDetailData)
+    //         setMovie({
+    //             posterImg: `${BASE_IMAGE_URL2}${movieDetailData.belongs_to_collection.poster_path}`,
+    //             backgroundImg: `${BASE_IMAGE_URL2}${movieDetailData.backdrop_path}`,
+    //             title: movieDetailData.title,
+    //             runtime: movieDetailData.runtime,
+    //             genres: movieDetailData.genres.map((el, index, array) => {
+    //                 if (index !== array.length - 1) {
+    //                     return el.name + ' | ';
+    //                 } else {
+    //                     return el.name;
+    //                 }
+    //             }).join(''),
+    //             releaseDate: movieDetailData.release_date,
+    //             voteAverage: movieDetailData.vote_average,
+    //             overView: movieDetailData.overview,
+    //         })
+    //       } catch (error) {
+    //         console.error('데이터 에러', error);
+    //       }
+    //     };
+    //     fetchData();
+    //   }, []);
 
+    useEffect(() => {
+        fetch(`${MOVIE_URL}${movieId}?api_key=${API_KEY}&language=ko-KR&page=1`)
+            .then((res) => res.json())
+            .then((res) => {
+                setMovie(res)
+            })
+    }, []);
+
+    const genres = movie.genres ? movie.genres.map((el) => el.name) : []
+    console.log(movie.voteAverage)
     return (
         <>
             <div className='blackBg'></div>
-            <img className="bgImg" src={movie.backgroundImg} alt={`${movie.title}`} />
+            <img className="bgImg" src={`${BASE_IMAGE_URL}${movie.backdrop_path}`} alt={`${movie.title}`} />
             <div className='detailContainer'>
 
                 <div className='movieInfo'>
@@ -47,14 +60,14 @@ const { movieId } = useParams()
                         <h1>{movie.title}</h1>
                         <div className='line'>
                             <span>{movie.releaseDate} 개봉</span>
-                            <span>{movie.genres}</span>
+                            <span>{genres}</span>
                         </div>
-                        <span>{movie.overView}</span>
+                        <span>{movie.overview}</span>
                     </div>
-                    <span className='voteAverage'>평점: {movie.voteAverage}점</span>
+                    <span className='voteAverage'>평점: {movie.vote_average}점</span>
                 </div>
 
-                <img className="poster" src={movie.posterImg} alt={`${movie.title}`} />
+                <img className="poster" src={`${BASE_IMAGE_URL}${movie.poster_path}`} alt={`${movie.title}`} />
             </div>
         </>
     );
