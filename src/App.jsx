e002e8b5648b497dd800380b777854ccc85c4588
hanhyochan <hcartist569@gmail.com>
2024-11-08 02:19:57 +0900

    mission2

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,18 @@
-import { useState, useEffect } from 'react'
 import './App.scss'
-import MovieCard from './component/MovieCard'
 import { Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
 import MovieDetail from './pages/MovieDetail'
 import NavBar from './component/NavBar '
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-const API_KEY = import.meta.env.VITE_API_KEY
 
 function App() {
-  const [movieList, setMovieList] = useState([])
-
-  const MOVIE_URL = 'https://api.themoviedb.org/3/movie/popular';
-  const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
-
-  useEffect(() => {
-    const fetchData = async () => {
-      fetch(`${MOVIE_URL}?api_key=${API_KEY}&language=ko-KR&page=1`)
-        .then((res) => res.json())
-        .then((res) => {
-          setMovieList(res.results.map((el) => ({
-            id: el.id,
-            img: BASE_IMAGE_URL + el.poster_path,
-            title: el.title,
-            voteAverage: el.vote_average
-          })))
-        })
-    };
-    fetchData();
-  }, []);
 
   return (
     <>
       <NavBar />
-      ff
       <Routes>
-        <Route path='/' element={<div className='container'>
-          {movieList.map((el) => <MovieCard key={el.id} movieList={el} />)}
-        </div>} />
+        <Route path='/' element={<Main />} />
         <Route path='/details/:movieId' element={<MovieDetail />} />
         <Route path='/signIn' element={<SignIn />} />
         <Route path='/signUp' element={<SignUp />} />
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,33 @@
-// import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 import './../App.scss'
 import MovieCard from '../component/MovieCard'
-// import movieListData from '././assets/data'
+const API_KEY = import.meta.env.VITE_API_KEY
 
 function Main() {
-    // const movieData = useSelector(state => state.movieList)
-console.log(movieListData)
+    const [movieList, setMovieList] = useState([])
+
+    const MOVIE_URL = 'https://api.themoviedb.org/3/movie/popular';
+    const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch(`${MOVIE_URL}?api_key=${API_KEY}&language=ko-KR&page=1`)
+                .then((res) => res.json())
+                .then((res) => {
+                    setMovieList(res.results.map((el) => ({
+                        id: el.id,
+                        img: BASE_IMAGE_URL + el.poster_path,
+                        title: el.title,
+                        voteAverage: el.vote_average
+                    })))
+                })
+        };
+        fetchData();
+    }, []);
 
     return (
         <div className='container'>
-            {movieData.map(el => <MovieCard key={el.id} movieList={el} />)}
+            {movieList.map(el => <MovieCard key={el.id} movieList={el} />)}
         </div>
     )
 }
