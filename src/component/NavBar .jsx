3b0ec3d4e397d40ceb2bd5e65869e9356e903b45
hanhyochan <hcartist569@gmail.com>
2024-11-12 18:32:19 +0900

    mission5

--- src/component/NavBar .jsx
+++ src/component/NavBar .jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useDebounce from '../hooks/useDebounce';
-import SearchBar from './searchBar';
+import SearchBar from './searchBar.jsx';
 import useMode from '../hooks/useMode';
 import AuthBtn from './authBtn';
 import Profile from './Profile';
