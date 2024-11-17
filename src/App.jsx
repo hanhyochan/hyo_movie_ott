c599ad9d9e84d60449e9e95e00e939d8b4cb4843
hanhyochan <hcartist569@gmail.com>
2024-11-17 11:57:56 +0900

    첫 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import './App.scss'
-import { Route, Routes } from 'react-router-dom'
-import Main from './pages/Main'
-import MovieDetail from './pages/MovieDetail'
-import SearchMovie from './pages/SearchMovie'
-import NavBar from './component/NavBar'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import { AuthProvider } from './context/AuthContext';
+import React from 'react';
+import { Routes ,Route } from 'react-router-dom';
 
-function App() {
-
+const App = () => {
   return (
     <div>
-      <AuthProvider>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/details/:movieId' element={<MovieDetail />} />
-          <Route path='/searchMovie' element={<SearchMovie />} />
-          <Route path='/signIn' element={<SignIn />} />
-          <Route path='/signUp' element={<SignUp />} />
-        </Routes >
-      </AuthProvider>
+      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
